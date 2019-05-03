25f4a6ff0a570ed3d12bb2acc49b53d02f942e34
Michael <mthomas22@gmail.com>
2019-05-03 13:37:05 -0400

    Started intro

--- AstrologySigns/Program.cs
+++ AstrologySigns/Program.cs
@@ -10,6 +10,26 @@ namespace AstrologySigns
     {
         static void Main(string[] args)
         {
+            //variables
+            string name;
+            int month;
+            int day;
+            int year;
+
+            //greeting screen
+            Console.WriteLine("\t \t Welcome To Sign Finder 3000!");
+            Console.WriteLine("\t We will ask you a couple of questions to determine \n \t what your astrology sign is and what that means for you!");
+            Console.WriteLine("\n\n\n \t Press any when ready!");
+            Console.ReadLine();
+
+            //gathering information
+            Console.Clear();
+            Console.WriteLine("Now to Find out a little bit about you.");
+            Console.WriteLine("Please type in your name.");
+            name = Console.ReadLine();
+            Console.WriteLine("Birthday");
+            month = Console.Read(); Console.Write("/"); day = Console.Read(); Console.Write("/"); year = Console.Read();
+
         }
     }
 }
